290d6c85052d0ef009acc226fecad53e860334d6
true_mogician <925269713@qq.com>
2022-04-22 12:22:55 +0800

    feat: replace deprecated methods

--- fabric/src/main/kotlin/me/lizardofoz/inventorio/ScreenTypeProviderFabric.kt
+++ fabric/src/main/kotlin/me/lizardofoz/inventorio/ScreenTypeProviderFabric.kt
@@ -2,16 +2,16 @@ package me.lizardofoz.inventorio
 
 import me.lizardofoz.inventorio.client.ui.InventorioScreen
 import me.lizardofoz.inventorio.player.InventorioScreenHandler
-import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry
 import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry
+import net.minecraft.client.gui.screen.ingame.HandledScreens
 import net.minecraft.screen.ScreenHandlerType
 import net.minecraft.util.Identifier
 
 object ScreenTypeProviderFabric : ScreenTypeProvider
 {
-    private val handlerProvider = ScreenHandlerRegistry.registerSimple(Identifier("inventorio", "player_screen")) { syncId, inv ->
-        InventorioScreenHandler(syncId, inv)
-    }
+    private val handlerProvider = ScreenHandlerRegistry.registerSimple(
+        Identifier("inventorio", "player_screen")
+    ) { syncId, inv -> InventorioScreenHandler(syncId, inv) }
 
     override fun getScreenHandlerType(): ScreenHandlerType<InventorioScreenHandler>
     {
@@ -20,6 +20,6 @@ object ScreenTypeProviderFabric : ScreenTypeProvider
 
     fun registerScreen()
     {
-        ScreenRegistry.register(handlerProvider) { handler, inventory, text -> InventorioScreen(handler, inventory) }
+        HandledScreens.register(getScreenHandlerType()){ handler, inventory, _ -> InventorioScreen(handler, inventory) }
     }
 }
